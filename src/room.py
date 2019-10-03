8e9013ae906bb58959069e4ebec4e1d14bd63257
AwaMelvine <melvineawa@gmail.com>
2019-10-03 17:43:48 +0100

    Print available items in room

--- src/room.py
+++ src/room.py
@@ -1,13 +1,26 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 import textwrap
+from item import Item
 
 
 class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.items = []
+        self.items = [Item('Apple', 'A fruit you can eat.'), Item(
+            'Ball', 'Kids and grown ups alike run after it and kick it away when they meet it')]
 
     def __str__(self):
-        return f"\nCurrent Room: {self.name}\nDescription: {textwrap.fill(self.description)}\n"
+        roomInfo = f"\nCurrent Room: {self.name}\nDescription: {textwrap.fill(self.description)}\n"
+
+        i = 1
+        if len(self.items) == 0:
+            roomInfo += f"\nNo items in this room."
+        else:
+            roomInfo += f"\nAvailable Items:"
+            for item in self.items:
+                roomInfo += f"\n{i}. {item.name}"
+                i += 1
+
+        return roomInfo + "\n"
